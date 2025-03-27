9b816732248406f337f41e270bf8cb4f24df27c2
Sandy Maguire <sandy@sandymaguire.me>
2025-03-26 18:53:38 -0700

    Cleaner Merkle tree

--- src/MerkleTree.hs
+++ src/MerkleTree.hs
@@ -7,24 +7,44 @@ import Data.Functor.Foldable.TH
 import Control.Comonad.Cofree
 
 data Tree a
-  = Leaf a
-  | Branch (Tree a) (Tree a)
+  = Empty
+  | Branch (Tree a) a (Tree a)
+
+
+insert :: Ord a => a -> Tree a -> Tree a
+insert a Empty = Branch Empty a Empty
+insert a (Branch l x r) =
+  case compare a x of
+    EQ -> Branch l x r
+    LT -> Branch (insert a l) x r
+    GT -> Branch l x (insert a r)
+
 
 makeBaseFunctor [''Tree]
 
-type Hash = Int
+
+data Hash
+
+instance Semigroup Hash
+instance Monoid Hash
 
 
-newtype Merkle f = Merkle
-  { getMerkle :: Cofree (Base f) Hash
-  }
+type Merkle f = Cofree (Base f) Hash
 
 summary :: Cofree f a -> a
 summary (a :< _) = a
 
-toMerkle :: Recursive f => (Base f Hash -> Hash) -> f -> Merkle f
-toMerkle alg = fmap Merkle $ cata $ \base ->
-  let hash = alg $ fmap summary base
-   in hash :< base
+toMerkle :: (Recursive f, Foldable (Base f)) => f -> Merkle f
+toMerkle = cata makeHash
 
+makeHash :: (Foldable f, Monoid a) => f (Cofree f a) -> Cofree f a
+makeHash base = foldMap summary base :< base
+
+insert' :: Ord a => a -> Cofree (Base (Tree a)) Hash -> Cofree (Base (Tree a)) Hash
+insert' a empty@(_ :< EmptyF) = makeHash $ BranchF empty a empty
+insert' a res@(_ :< BranchF l x r) =
+  case compare a x of
+    EQ -> res
+    LT -> makeHash $ BranchF (insert' a l) x r
+    GT -> makeHash $ BranchF l x (insert' a r)
 
