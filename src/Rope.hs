99801817fa0c9f192705e9f248e502a553e2fdb4
Sandy Maguire <sandy@sandymaguire.me>
2025-03-26 20:49:09 -0700

    Split rope

--- src/Rope.hs
+++ src/Rope.hs
@@ -1,7 +1,9 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
 
 module Rope where
 
+import GHC.Exts
 import Data.Functor.Foldable
 import Data.Functor.Foldable.TH
 import Control.Comonad.Cofree
@@ -19,6 +21,9 @@ makeBaseFunctor [''StringTree]
 summary :: Cofree f a -> a
 summary (a :< _) = a
 
+value :: Cofree f a -> f (Cofree f a)
+value (_ :< f) = f
+
 type Rope = Cofree StringTreeF Int
 
 
@@ -26,9 +31,29 @@ type Rope = Cofree StringTreeF Int
 index :: Base StringTree Rope -> Int -> Maybe Char
 index (LeafF str) ix = listToMaybe $ drop ix str
 index (BranchF (lsz :< l) (_ :< r)) ix =
-  case ix <= lsz of
+  case ix < lsz of
     True -> index l ix
     False -> index r $ ix - lsz
+
+split :: Rope -> Int -> (Rope, Rope)
+split r@(sz :< _) ix
+  | ix >= sz = (r, mempty)
+split (_ :< LeafF str) ix =
+  let (top, bot) = splitAt ix str
+    in (rope top, rope bot)
+split (_ :< BranchF l@(lsz :< _) r) ix =
+  case ix < lsz of
+    True ->
+      let (top, bot) = split l ix
+       in (top, bot <> r)
+    False ->
+      let (top, bot) = split r $ ix - lsz
+       in (l <> top, bot)
+
+toStr :: Rope -> String
+toStr (_ :< LeafF str) = str
+toStr (_ :< BranchF l r) = toStr l <> toStr r
+
 
 instance Semigroup Rope where
   (0 :< _) <> r = r
@@ -38,3 +63,12 @@ instance Semigroup Rope where
 instance Monoid Rope where
   mempty = 0 :< LeafF ""
 
+rope :: String -> Rope
+rope s = length s :< LeafF s
+
+
+instance IsString Rope where
+  fromString = rope
+
+test :: Rope
+test = (("Hello " <> "my ") <> (("na" <> "me i") <> ("s" <> " Sandy"))) <> ""
